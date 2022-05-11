bb8812eaf90c3f16ded8caa70d187dde057a580c
Emmanuel F. Werr <47487994+emmanuelfwerr@users.noreply.github.com>
2022-05-12 00:50:36 +0200

    completed preu collector and persistence loader

--- BDM/P2/opendatabcn_persistent.py
+++ BDM/P2/opendatabcn_persistent.py
@@ -5,13 +5,14 @@ import glob
 import os
 
 '''
-This script concatenates all individual yearly files extracted by `opendatabcn_accidents_collector.py` and produces a unified .json file with all the extracted accidents information, then exports it to the persistent landing zone.
+This script concatenates all individual yearly files extracted by `accidents_collector.py` and `preu_collector.py` and produces two unified .json files with all the extracted accidents information, then exports them to the persistent landing zone.
 Args:
     (none)
 Returns:
     (.json) accidents_opendata.json - .json file containing all extracted data opendatabcn-accidents
+    (.json) preu_opendata.json - .json file containing all extracted data opendatabcn-lloguer_preu
 '''
-
+# accidents opendatabcn
 path = r'landing/temporal/opendatabcn-accidents/'
 all_files = glob.glob(path + "/*.csv")
 
@@ -24,3 +25,17 @@ for filename in all_files:
 df = pd.concat(li, axis=0, ignore_index=True)
 
 df.to_json('landing/persistent/accidents_opendata/accidents_opendata.json', orient='records')
+
+# lloguer preu opendatabcn
+path = r'landing/temporal/opendatabcn-lloguer_preu/'
+all_files = glob.glob(path + "/*.csv")
+
+li = []
+
+for filename in all_files:
+    df = pd.read_csv(filename)
+    li.append(df)
+
+df = pd.concat(li, axis=0, ignore_index=True)
+
+df.to_json('landing/persistent/preu_opendata/preu_opendata.json', orient='records')
---
+++ BDM/P2/preu_collector.py
@@ -0,0 +1,101 @@
+import pandas as pd
+from datetime import date
+import os
+from bs4 import BeautifulSoup
+import requests
+
+
+def get_filepaths(directory):
+    '''
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    '''
+    file_names = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            file_names.append(filename)  # Add it to the list.
+
+    return file_names  # Output
+
+
+def check_update_years(files):
+    '''
+    
+    '''
+    current_year = date.today().year # get current year
+    update_years = [str(year) for year in range(2014, current_year)] # opendatabcn-lloguer_preu available years
+    
+    # Check years present in topic's current landing zone
+    present_years = [element[:4] for element in files] # returns a list of present years
+    
+    #
+    for element in present_years:
+        if element in update_years:
+            update_years.remove(element)
+    
+    return update_years
+
+
+def collect_data(update_years):
+    '''
+    
+    '''
+    # Input page link
+    link = 'https://opendata-ajuntament.barcelona.cat/data/es/dataset/est-mercat-immobiliari-lloguer-mitja-mensual'
+
+    # Request HTML for the link
+    html_text = requests.get(link).text
+    soup = BeautifulSoup(html_text, features="html.parser")
+    # Parse link HTML
+    yearly_reports = soup.find('ul', {'class': 'resource-list'})
+    report_links = yearly_reports.findAll('a', {'class':'heading'}, href=True)
+
+    # extracting report urls from dataset page
+    yearly_reports = {}
+    for i in report_links:
+        yearly_reports[i.text.strip()[:-3].lower()] = 'https://opendata-ajuntament.barcelona.cat{}'.format(i['href'].strip())
+
+    # removing 'xml' datasets and previously-extracted yearly reports
+    yearly_reports = {key[:4]:val for key, val in yearly_reports.items() if key[-3:] != 'xml' and key[:4] in update_years}
+
+    # extract pending yearly reports
+    for key, value in yearly_reports.items():
+        # Request HTML for the link
+        html_text = requests.get(value).text
+        soup = BeautifulSoup(html_text, features="html.parser")
+        # Parse link HTML
+        download_section = soup.find('div', {'class': 'actions resource-actions'})
+        reports = download_section.findAll('a', {'class':'resource-type-None'}, href=True)
+        # progress-tracker(UI)
+        print('\nExtracting report for {}...'.format(key))
+
+        for i in reports:
+            try: # more recent reports dont need to specify encoding
+                df = pd.read_csv(str(i['href']))
+            except: # I know this is ugly but it is necessary
+                try: # some reports use ';' as delimiter
+                    df = pd.read_csv(str(i['href']), encoding='unicode_escape')
+                except:
+                    print('*** This report uses semicolon (;) as delimiter *** accomodating...')
+                    df = pd.read_csv(str(i['href']), encoding='unicode_escape', sep=';')
+            df.to_csv('landing/temporal/opendatabcn-lloguer_preu/{}_lloguer_preu_bcn.csv'.format(key), encoding='utf-8', index=False)
+            # progress-tracker(UI)
+            print('{}_lloguer_preu_bcn.csv exported to temporal landing zone'.format(key))
+
+
+if __name__ == '__main__':
+    #
+    opendatabcn_filenames = get_filepaths("landing/temporal/opendatabcn-lloguer_preu")
+    update_years = check_update_years(opendatabcn_filenames)
+    
+    # opendatabcn data collection
+    if update_years:
+        print('\nlloguer preu data from opendatabcn is not up to date!')
+        collect_data(update_years)
+        print('\nUpdate Complete!\n')
+    else:
+        print('\nlloguer preu data from opendatabcn is already up to date!\n')
